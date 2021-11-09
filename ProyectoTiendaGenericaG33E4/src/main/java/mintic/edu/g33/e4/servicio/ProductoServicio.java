f5f5cc5ff1370fa8e10177936b0fea7fd15b4c0e
migihf <90869962+migihf@users.noreply.github.com>
2021-11-09 14:44:27 -0500

    Update ProductoServicio.java

--- ProyectoTiendaGenericaG33E4/src/main/java/mintic/edu/g33/e4/servicio/ProductoServicio.java
+++ ProyectoTiendaGenericaG33E4/src/main/java/mintic/edu/g33/e4/servicio/ProductoServicio.java
@@ -2,39 +2,40 @@ package mintic.edu.g33.e4.servicio;
 
 import java.util.List;
 
-import org.springframework.transaction.annotation.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import mintic.edu.g33.e4.modelo.ProductoRepositorio;
 import mintic.edu.g33.e4.modelo.Productos;
 
+/*
+ * Servicio de Producto
+ * 
+ * Funciones: 
+ * lista, editar, crear, eliminar 
+ * 
+ */
+
 @Service
 @Transactional
 public class ProductoServicio {
-	
 	@Autowired
 	private ProductoRepositorio repo;
-	
-	//Listado de Productos
-	public List<Productos> listar(){
+
+	public List<Productos> consultarListaProducto(){
 		return repo.findAll();
 	}
-	
-	//Consultar Producto por codigo
-	public Productos listarId(int id) {
-		return repo.findById(id).get();
+
+	public Productos consultarProducto(int cod) {
+		return repo.findById(cod).get();
 	}
-	
-	//Guardar datos de Producto
-	public void save(Productos p) {
-		repo.save(p);
+
+	public void editarProducto(Productos pro) {
+		repo.save(pro);
 	}
-	
-	//Eliminar Producto
-	public void delete(int d) {
-		repo.deleteById(d);
+
+	public void eliminarProducto(int cod) {
+		repo.deleteById(cod);
 	}
-	
 }
